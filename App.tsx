d625027be3f7df46d13c17f56ffe3252ebf0ed0c
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-25 15:36:38 +0200

    rename expo-av -> expo-av-jsi

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 import MusicPicker from "./custom_native_modules/expo-music-picker/src/MusicPicker";
-import { Audio } from "./custom_native_modules/expo-av/src";
+import { Audio } from "./custom_native_modules/expo-av-jsi/src";
 import Reanimated, {
   Extrapolate,
   interpolate,
