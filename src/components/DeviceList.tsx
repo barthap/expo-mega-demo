f7aca31e497541c7b7eb9b646ba7a87646e345ee
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-26 12:19:58 +0200

    Connecting/disconnecting basics

--- src/components/DeviceList.tsx
+++ src/components/DeviceList.tsx
@@ -3,27 +3,39 @@ import { Device } from "react-native-ble-plx";
 import { FlatList } from "react-native-gesture-handler";
 import { ListRenderItem, Text } from "react-native";
 import { StyleSheet } from "react-native";
-import { Divider, List, ListItem } from "@ui-kitten/components";
+import { Divider, List, ListItem, Button } from "@ui-kitten/components";
 
 interface Props {
   devices: Device[];
-  onDeviceClick?: () => void;
+  connectedDeviceId?: string | null;
+  onConnectClick?: (deviceId: string) => void;
   onScanRefresh?: () => void;
   isScanning?: boolean;
 }
 
-const RenderItem: ListRenderItem<Device> = ({ item }) => {
-  return (
-    <ListItem title={item.name ?? "[No name]"} description={`ID: ${item.id}`} />
-  );
-};
-
 export function DeviceList(props: Props) {
+  const renderItemAccessory = (id) => () =>
+    (
+      <Button size="tiny" onPress={() => props.onConnectClick?.(id)}>
+        Connect
+      </Button>
+    );
+
+  const renderItem: ListRenderItem<Device> = ({ item }) => (
+    <ListItem
+      title={item.name ?? "[No name]"}
+      description={`ID: ${item.id}`}
+      accessoryRight={
+        props.connectedDeviceId !== item.id && renderItemAccessory(item.id)
+      }
+    />
+  );
+
   return (
     <List
       data={props.devices}
       keyExtractor={(item) => item.id}
-      renderItem={RenderItem}
+      renderItem={renderItem}
       ItemSeparatorComponent={Divider}
       refreshing={props.isScanning ?? false}
       onRefresh={props.onScanRefresh}
--- src/screens/BluetoothScreen.tsx
+++ src/screens/BluetoothScreen.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { BluetoothManager } from "../BluetoothManager";
 import { Device } from "react-native-ble-plx";
 import create from "zustand";
 import { produce } from "immer";
 import { DeviceList } from "../components/DeviceList";
-import { Layout, Text } from "@ui-kitten/components";
+import { Layout, Text, Button, Card } from "@ui-kitten/components";
 
-type State = { devices: Record<string, Device>; isScanning: boolean };
+type State = {
+  devices: Record<string, Device>;
+  isScanning: boolean;
+  connectedDevice: Device | null;
+};
 const useDevicesStore = create<State>((set, get) => ({
   devices: {},
+  connectedDevice: null,
   isScanning: false,
 }));
 
@@ -29,7 +34,6 @@ function doDeviceScan() {
       state.devices[device.id] = device;
     });
     console.log("Found", device.name, device.id);
-    // devices[device.id] = device;
   });
 
   useDevicesStore.update((state) => {
@@ -47,6 +51,30 @@ function stopScanning() {
     state.isScanning = false;
   });
   BluetoothManager.stopDeviceScan();
+}
+
+async function connect(deviceToConnect: Device) {
+  const connected = await deviceToConnect.connect();
+  const device = await connected.discoverAllServicesAndCharacteristics();
+  console.log("Connected to", device.name);
+  console.log(
+    "  Services:",
+    (await device.services()).map((s) => s.id)
+  );
+  useDevicesStore.update((state) => {
+    state.connectedDevice = device;
+  });
+}
+
+function disconnect() {
+  const dev = useDevicesStore.getState().connectedDevice;
+  if (!dev) return;
+
+  BluetoothManager.cancelDeviceConnection(dev.id);
+  useDevicesStore.update((state) => {
+    state.connectedDevice = null;
+  });
+  console.log("Disconnected");
 }
 
 export default function BluetoothScreen() {
@@ -75,15 +103,68 @@ export default function BluetoothScreen() {
     return stopScanning;
   }, []);
 
-  const deviceInfos = useDevicesStore((state) => state);
+  const store = useDevicesStore((state) => state);
 
   return (
-    <Layout style={{ flex: 1 }}>
+    <Layout style={styles.container} level="2">
       <DeviceList
-        devices={Object.values(deviceInfos.devices)}
-        isScanning={deviceInfos.isScanning}
+        devices={Object.values(store.devices)}
+        isScanning={store.isScanning}
         onScanRefresh={doDeviceScan}
+        onConnectClick={(id) => connect(store.devices[id])}
+        connectedDeviceId={store.connectedDevice?.id ?? null}
       />
+
+      <Card style={styles.card} header={Header} footer={Footer}>
+        <Text>
+          {JSON.stringify(store.connectedDevice?.manufacturerData, null, 2)}
+        </Text>
+      </Card>
     </Layout>
   );
 }
+
+const Header = (props) => {
+  const dev = useDevicesStore((state) => state.connectedDevice);
+  return (
+    <View {...props}>
+      <Text category="h6">{dev?.name || "Not connected"}</Text>
+      <Text category="s1">{dev?.id}</Text>
+    </View>
+  );
+};
+
+const Footer = (props) => {
+  const dev = useDevicesStore((state) => state.connectedDevice);
+  return (
+    <View {...props} style={[props.style, styles.footerContainer]}>
+      <Button
+        style={styles.footerControl}
+        size="small"
+        disabled={dev == null}
+        onPress={disconnect}
+      >
+        DISCONNECT
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  card: {
+    // flex: 1,
+    margin: 5,
+  },
+  footerContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  footerControl: {
+    marginHorizontal: 2,
+  },
+});
