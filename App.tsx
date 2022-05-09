dcfd007cc4d9a804e9d9cbe75282ece686ddb72d
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-05-09 09:24:18 +0200

    Fix dev-client on ios

---
+++ App.tsx
@@ -0,0 +1,5 @@
+import "expo-dev-client";
+import "react-native-gesture-handler";
+
+import App from "./src/App";
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import 'expo-dev-client';
+import { registerRootComponent } from 'expo';
 
-import "react-native-gesture-handler";
-import { registerRootComponent } from "expo";
-
-import App from "./src/App";
+import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
