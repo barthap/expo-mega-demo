e046af6255c73bd320b48af59b538d80f40cbefd
Nic Redshaw <nic@nicredshaw.plus.com>
2022-12-28 09:54:32 +0000

    Fix colour picker screen using new 'Gesture' and 'GestureDetector' from RNGH2

--- src/components/picker/Picker.tsx
+++ src/components/picker/Picker.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable max-len */
 import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
-import { PanGestureHandler } from "react-native-gesture-handler";
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 import Svg, { Path } from "react-native-svg";
 import Animated, {
   pow,
   SharedValue,
   useSharedValue,
-  useAnimatedGestureHandler,
   useAnimatedStyle,
   runOnJS,
   useAnimatedProps,
@@ -34,42 +33,14 @@ interface PickerProps {
 }
 
 export default ({ h, s, onGestureEnd, backgroundColor }: PickerProps) => {
-  const x = useSharedValue(CENTER.x);
-  const y = useSharedValue(CENTER.y);
-
-  const eventHandler = useAnimatedGestureHandler({
-    onStart: (_, ctx: { startX: number; startY: number }) => {
-      ctx.startX = x.value;
-      ctx.startY = y.value;
-    },
-    onActive: (event, ctx) => {
-      const newX = ctx.startX + event.translationX;
-      const newY = ctx.startY + event.translationY;
-      const polar = canvas2Polar({ x: newX, y: newY }, CENTER);
-      const l = {
-        theta: polar.theta,
-        radius: clamp(polar.radius, 0, CANVAS_SIZE / 2),
-      };
-      const xy = polar2Canvas(l, CENTER);
-      x.value = xy.x;
-      y.value = xy.y;
-
-      const hue = (l.theta % (2 * Math.PI)) / (2 * Math.PI);
-      const saturation = l.radius === 0 ? 0 : l.radius / (CANVAS_SIZE / 2);
-
-      h.value = hue;
-      s.value = saturation * saturation;
-    },
-    onEnd: () => {
-      runOnJS(onGestureEnd)();
-    },
-  });
-
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({ x: CENTER.x, y: CENTER.y });
+  
   const pickerStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        { translateX: x.value - PICKER_WIDTH / 2 },
-        { translateY: y.value - PICKER_HEIGHT / 2 },
+        { translateX: offset.value.x - PICKER_WIDTH / 2 },
+        { translateY: offset.value.y - PICKER_HEIGHT / 2 },
       ],
     };
   });
@@ -80,9 +51,45 @@ export default ({ h, s, onGestureEnd, backgroundColor }: PickerProps) => {
     };
   });
 
+  const start = useSharedValue({ x: CENTER.x, y: CENTER.y });
+  const gesture = Gesture.Pan()
+      .onBegin(() => {
+          isPressed.value = true;
+      })
+      .onUpdate((e) => {
+          const newX = e.translationX + start.value.x;
+          const newY = e.translationY + start.value.y;
+          const polar = canvas2Polar({ x: newX, y: newY }, CENTER);
+          const l = {
+            theta: polar.theta,
+            radius: clamp(polar.radius, 0, CANVAS_SIZE / 2),
+          };
+          const xy = polar2Canvas(l, CENTER);
+          offset.value = {
+            x: xy.x,
+            y: xy.y,
+          };
+          const hue = (l.theta % (2 * Math.PI)) / (2 * Math.PI);
+          const saturation = l.radius === 0 ? 0 : l.radius / (CANVAS_SIZE / 2);
+
+          h.value = hue;
+          s.value = saturation * saturation;
+      })
+      .onEnd(() => {
+          start.value = {
+            x: offset.value.x,
+            y: offset.value.y,
+          };
+          runOnJS(onGestureEnd)();
+      })
+      .onFinalize(() => {
+          isPressed.value = false;
+      });
+
+
   return (
     <View style={StyleSheet.absoluteFill}>
-      <PanGestureHandler onGestureEvent={eventHandler}>
+      <GestureDetector gesture={gesture}>
         <Animated.View
           style={[
             {
@@ -109,13 +116,14 @@ export default ({ h, s, onGestureEnd, backgroundColor }: PickerProps) => {
             <AnimatedPath
               d="M22 .889C9.943.889.167 10.664.167 22.723.167 37.127 22 59.111 22 59.111S43.833 37.43 43.833 22.723C43.833 10.664 34.057.889 22 .889z"
               animatedProps={animatedProps}
+              fill="#ff0000"  // Provide initial colour to avoid '-1 is not a valid color or brush' warning
               stroke="#fff"
               strokeWidth={STROKE_WIDTH}
               fillRule="evenodd"
             />
           </Svg>
         </Animated.View>
-      </PanGestureHandler>
+      </GestureDetector>
     </View>
   );
 };
--- src/screens/RGBControlScreen.tsx
+++ src/screens/RGBControlScreen.tsx
@@ -4,6 +4,7 @@ import { Layout, Text, Button, Input, Icon } from "@ui-kitten/components";
 import shallow from "zustand/shallow";
 import { useDevicesStore } from "../bluetooth/BluetoothManager";
 import { isDeviceSupported, sendCommandTo } from "../bluetooth/BluetoothDevice";
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import ColorPicker from "../components/picker";
 
 export default function RGBControlScreen() {
@@ -30,44 +31,46 @@ export default function RGBControlScreen() {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      style={{ flex: 1 }}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 90 : 0}
-    >
-      <Layout style={styles.container} level="2">
-        {isConnected ? (
-          <Text status="success" category="s2">
-            Connected to {device.name ?? "[no name]"}
-          </Text>
-        ) : (
-          <Text status="danger" category="s2">
-            Connect to a Bluetooth device first!
-          </Text>
-        )}
-        <ColorPicker onColorChange={sendRgb} />
+    <GestureHandlerRootView style={{flex: 1}}>  
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 90 : 0}
+      >
+        <Layout style={styles.container} level="2">
+          {isConnected ? (
+            <Text status="success" category="s2">
+              Connected to {device.name ?? "[no name]"}
+            </Text>
+          ) : (
+            <Text status="danger" category="s2">
+              Connect to a Bluetooth device first!
+            </Text>
+          )}
+          <ColorPicker onColorChange={sendRgb} />
 
-        <Layout level="3" style={styles.formGroup}>
-          <Input
-            style={styles.input}
-            placeholder="Enter command here"
-            // value={command}
-            onChangeText={setCommand}
-            onSubmitEditing={sendCommand}
-            disabled={!isConnected}
-          />
-          <Button
-            size="small"
-            style={{ margin: 3 }}
-            onPress={sendCommand}
-            disabled={!isConnected}
-            accessoryRight={<Icon name="corner-down-right-outline" />}
-          >
-            Send
-          </Button>
+          <Layout level="3" style={styles.formGroup}>
+            <Input
+              style={styles.input}
+              placeholder="Enter command here"
+              // value={command}
+              onChangeText={setCommand}
+              onSubmitEditing={sendCommand}
+              disabled={!isConnected}
+            />
+            <Button
+              size="small"
+              style={{ margin: 3 }}
+              onPress={sendCommand}
+              disabled={!isConnected}
+              accessoryRight={<Icon name="corner-down-right-outline" />}
+            >
+              Send
+            </Button>
+          </Layout>
         </Layout>
-      </Layout>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </GestureHandlerRootView>
   );
 }
 
